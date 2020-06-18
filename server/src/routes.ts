638750e4b0c150535f9a68b4c70c0c6b5bf40f16
lucasalvine <lucas.alvine@thinkseg.com>
2020-06-18 16:58:12 -0300

    routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 
+import PointsController from './controllers/PointsController';
+import ItemsController from './controllers/ItemsController';
+
 const routes = express.Router();
+const pointsController = new PointsController;
+const itemsController = new ItemsController;
 
-routes.get('/users', (req, res) => {
+routes.get('/items', itemsController.index);
 
-  return res.json({ message: "hello" });
-});
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import routes from './routes';
+import path from 'path';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
+
+app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3000);
-
-//npx ts-node => exec file
-//npx ts-node-dev => exec file like nodemon
