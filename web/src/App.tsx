490657ddc74fedc6579d13847468df6324fb74a2
lucasalvine <lucas.alvine@thinkseg.com>
2020-06-24 17:54:39 -0300

    web - react

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <p>
+      Edit <code>src/App.js</code> and save to reload.
+    </p>
+  );
+}
+
+export default App;
